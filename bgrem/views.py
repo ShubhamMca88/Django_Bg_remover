613f65c29953b524b637f82fc0f48d72fd6aebcc
Shubham <byshubham6@gmail.com>
2024-06-02 16:58:45 +0530

    #

--- bgrem/views.py
+++ bgrem/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from PIL import Image
+from rembg import remove
+import io
+import base64
 
-# Create your views here.
+def home(request):
+    return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+@csrf_exempt
+def home(request):
+    if request.method == 'POST':
+        image_file = request.FILES['image']
+        img = Image.open(image_file)
+        output = remove(img)
+        buffer = io.BytesIO()
+        output.save(buffer, format="PNG")
+        img_str = base64.b64encode(buffer.getvalue()).decode()
+        return JsonResponse({'image': img_str})
+    return render(request, 'main/home.html')
